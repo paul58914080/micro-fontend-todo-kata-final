96c359526e0d3ce1e70583e170fb634b4b6583c4
paul58914080 <paul58914080@gmail.com>
2019-07-25 10:50:43 +0530

    feat(app): integrated create & view micro-app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ViewTodoComponent} from '../../../projects/view-todo/src/app/view-todo.component';
 
 @Component({
   selector: 'todo-home',
@@ -8,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   loadElement = false;
+  @ViewChild(ViewTodoComponent, { static: false }) view: ViewTodoComponent;
 
   constructor() { }
 
   ngOnInit() {
+    this.loadElement = false;
   }
 
+  created() {
+    this.view.actionChanged(this.view.selectedAction);
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,13 @@ import {CreateTodoModule} from '../../../projects/create-todo/src/app/create-tod
 import {ViewTodoModule} from '../../../projects/view-todo/src/app/view-todo.module';
 
 @NgModule({
-  declarations: [ HomeComponent ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     CreateTodoModule,
     ViewTodoModule
-  ]
+  ],
+  bootstrap: [HomeComponent]
 })
 export class HomeModule {
 }
